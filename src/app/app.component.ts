5ead84c3877ce3fe505b2bea855fd9189c00f88e
Caleb Devonport <calebdevonport@outlook.com>
2021-01-18 23:03:42 +0000

    Added media queries and code to check for if on mobile or desktop and modify view accordingly (in future need to make this change on the fly when resizing window)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'calebdevonport-webapp';
-  value = false;
+export class AppComponent  {
+  public get isMobile(): boolean {
+    const query = '(max-width: 768px)';
+    const mediaQueryList = window.matchMedia(query);
+
+    return mediaQueryList.matches ? true : false;
+  }
+
+  public get mode(): string {
+    return this.isMobile ? 'over' : 'side';
+  } 
 }
